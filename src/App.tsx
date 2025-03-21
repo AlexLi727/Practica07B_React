98694922c8f7365cb8309e082152ba501e62dc93
kaimex9 <kaimexalabau@gmail.com>
2025-03-21 15:44:42 +0100

    3rd component design

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Banner from "./components/comp_menu/Banner/banner";
 import "./App.css";
 import Card from "./components/comp_menu/Card/card";
 import Footer from "./components/comp_menu/Footer/footer";
 import Button from "./components/comp_menu/Button/button";
 import DynamicForm from "./components/comp_formularios/dynamicForm";
+import result from "./components/comp_result/result";
 
 const App: React.FC = () => {
   const [component, setComponent] = useState(0);
   const title = "Práctica 07B - Formularios con REACT";
-  const footerAppName= "Quizly";
+  const footerAppName = "Quizly";
 
   const changeComponent = () => {
     console.log("hola");
     setComponent(1);
   }
 
-  switch(component){
+  switch (component) {
     case 0:
       return (
         <>
           <div className="body">
-            <section className = "card">
-            <Banner title={title} />
-            <Card />
-            <Button onClick={changeComponent} />
-            <Footer footerAppName={footerAppName}/>
+            <section className="card">
+              <Banner title={title} />
+              
+              {/* <Card />
+              <Button onClick={changeComponent} /> */}
+              <Footer footerAppName={footerAppName} />
             </section>
           </div>
-    
+
         </>
       );
     case 1:
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.tsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './i18n';
+import Result from './components/comp_result/result';
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    
+    <Result />
   </StrictMode>,
 )
